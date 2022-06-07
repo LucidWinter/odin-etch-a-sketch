3d92053806bd9f4f44052a1dcb1557c5e0a46c6b
LucidWinter <mike.brown11311@gmail.com>
2022-06-07 10:51:04 -0400

    User can set grid size, hovering color is random

--- js/script.js
+++ js/script.js
@@ -1,23 +1,55 @@
 const container = document.querySelector('.container-box');
 
-function createBlock() {
-    const block = document.createElement('div');
-    block.classList.add('block');
-    block.addEventListener('mouseenter', gridHoverEffect);
+function defaultGrid(){
+    let defaultSize = 8 * 8
 
-    container.appendChild(block);
-}
-
-function createGrid() {
-    let gridSize = 16 * 16;
-
-    for (let i = 0; i < gridSize; i++) {
-        createBlock();
+    for (let i = 0; i < defaultSize; i++) {
+        createBlock(defaultSize);
     }
 }
 
-function gridHoverEffect(){
-    this.style.backgroundColor = '#00FF00';
+function setGridSize(){
+    let size = prompt('Enter in the length of the grid size (value must be between 2 and 50)');
+
+    if(size > 50 || size < 2){
+        setGridSize();
+    }
+
+    gridSize = +size * +size;
+    return gridSize;
 }
 
-createGrid();
+function createGrid(gridSize) {
+    for (let i = 0; i < gridSize; i++) {
+        createBlock(gridSize);
+    }
+}
+
+function createBlock(gridSize) {
+    const block = document.createElement('div');
+    const containerSize = 640 * Math.sqrt(gridSize);
+    const blockSize = containerSize/gridSize;
+
+    block.classList.add('block');
+    block.style.width = `${blockSize}px`;
+    block.style.height = `${blockSize}px`;
+    block.addEventListener('mouseenter', gridHoverEffect);
+    container.appendChild(block);
+}
+
+function gridHoverEffect(){
+    let o = Math.round, r = Math.random, s = 255;
+    this.style.backgroundColor = 'rgba(' + o(r()*s) + ',' + o(r()*s) + ',' + o(r()*s) + ',' + r().toFixed(1) + ')';
+}
+
+function resetGrid(){
+    container.innerHTML = '';
+}
+
+function runEverything(){
+    setGridSize();
+    resetGrid();
+    createGrid(gridSize);
+}
+
+defaultGrid();
