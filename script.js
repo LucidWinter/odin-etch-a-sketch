d5692b224e97f4520cb88874167307fa97bb866e
LucidWinter <mike.brown11311@gmail.com>
2025-04-21 13:36:27 -0400

    Fixed grid layout bug + added proper hover effect
    grid would sometimes not calculate right and push boxes onto another level.
    squares properly change color only when moused over

--- script.js
+++ script.js
@@ -1,17 +1,25 @@
 const grid = document.querySelector("#grid");
+let box = document.createElement("div");
+
 
 function generateGrid(size){
     let amountOfBoxes = size * size;
     let boxWidthHeight = (1000/size) - 2;
-
+    
     for(let i = 0; i < amountOfBoxes; i++){
-        const box = document.createElement("div");
-        box.classList.add("box", ".box:hover");
+        box = document.createElement("div")
+        box.classList.add("box");
         box.style.height = `${boxWidthHeight}px`;
         box.style.width = `${boxWidthHeight}px`;
         grid.appendChild(box);
     }
-
 }
 
-generateGrid(16);
+generateGrid(32);
+let boxes = document.querySelectorAll(".box");
+
+boxes.forEach((square) =>{
+    square.addEventListener("mouseenter", () =>{
+        square.style.backgroundColor = "black"
+    })
+})
