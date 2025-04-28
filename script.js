bb49a8b02ed0b4a1f13f6a27a3f2551a7bc672b1
LucidWinter <mike.brown11311@gmail.com>
2025-04-28 12:20:17 -0400

    Remove old grid when new grid is created

--- script.js
+++ script.js
@@ -1,10 +1,17 @@
 const grid = document.querySelector("#grid");
+const btn = document.querySelector("#btn");
 let box = document.createElement("div");
 
+function setGridSize(){
+    let size = prompt("Please enter in the grid size(2-100)")
+    generateGrid(size);
+}
 
 function generateGrid(size){
+    grid.innerHTML = "";
     let amountOfBoxes = size * size;
     let boxWidthHeight = (1000/size) - 2;
+    
     
     for(let i = 0; i < amountOfBoxes; i++){
         box = document.createElement("div")
@@ -13,13 +20,19 @@ function generateGrid(size){
         box.style.width = `${boxWidthHeight}px`;
         grid.appendChild(box);
     }
+
+    let boxes = document.querySelectorAll(".box");
+
+    boxes.forEach((square) =>{
+        square.addEventListener("mouseenter", () =>{
+            square.style.backgroundColor = "black"
+        })
+    })
 }
 
-generateGrid(32);
-let boxes = document.querySelectorAll(".box");
 
-boxes.forEach((square) =>{
-    square.addEventListener("mouseenter", () =>{
-        square.style.backgroundColor = "black"
-    })
-})
+generateGrid(16)
+btn.addEventListener("click", setGridSize);
+
+
+
